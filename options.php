f89a7ae2d9bcfa54a7daaace83e65b6bed60fce2
Jason Dark <jason@jasondarkx2.com>
2015-09-12 15:45:26 -0400

    Documentation clean up

--- options.php
+++ options.php
@@ -1,15 +1,9 @@
 <?php
 /*
-Plugin Name: Wordpress Widget Manager
-Plugin URI: https://github.com/JasonDarkX2/Wordpress-WidgetManager
-Description:Simply a Wordpress Plugin dedicated to help easily manage custom and default Wordpress widgets
+admin options file for Widget Manager
 Author:Jason Dark X2
-version: 0.1
 Author URI:http://www.jasondarkx2.com/ 
-*/ 
-?>
-<?php
- 
+*/  
 $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
  require_once( $parse_uri[0] . 'wp-load.php' );
 $enabled= get_option('enabled_widgets');
--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,6 @@
 <?php
 /*
-Plugin Name: Wordpress Widget Manager
-Plugin URI: https://github.com/JasonDarkX2/Wordpress-WidgetManager
-Description:Simply a Wordpress Plugin dedicated to help easily manage custom and default Wordpress widgets
+Uninstall script for Widget Manager
 Author:Jason Dark X2
 version: 0.1
 Author URI:http://www.jasondarkx2.com/ 
