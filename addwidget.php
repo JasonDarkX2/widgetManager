f22616c7f1103d7824f00e94b4610caf20e08539
Jason Dark <jason@jasondarkx2.com>
2015-11-01 23:25:01 -0500

    added directory and upload file code
    *not fully fuctional yet*

--- addwidget.php
+++ addwidget.php
@@ -5,5 +5,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-echo "hello";
+$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+ require_once( $parse_uri[0] . 'wp-load.php' );
+$target_dir =plugin_dir_path( __FILE__ ).'custom-widgets/';
+//echo $target_dir;
+echo $_FILES['widgetToUpload']['name'];
+//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
