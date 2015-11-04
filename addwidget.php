96cb311ab6f54b8715bf1e1daabddf4bae44412f
Jason Dark <jason@jasondarkx2.com>
2015-11-03 22:55:03 -0500

    implemented the add/import custom widget  option

--- addwidget.php
+++ addwidget.php
@@ -10,4 +10,25 @@ $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
 $target_dir =plugin_dir_path( __FILE__ ).'custom-widgets/';
 //echo $target_dir;
 echo $_FILES['widgetToUpload']['name'];
-//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+if(!empty($_FILES)){
+    var_dump($_FILES);
+    $target_file = $target_dir . basename($_FILES["widgetToUpload"]['name']);
+$upload = 1;
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $upload = 0;
+}
+if ($upload == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["widgetToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["widgetToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+}
+if(empty($_FILES)){
+    echo '200';
+}
