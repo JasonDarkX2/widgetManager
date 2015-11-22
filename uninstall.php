918fd4c3e582e3c14dfb9a08bf2ec6001c4720b7
Jason Dark <jason@jasondarkx2.com>
2015-11-21 23:24:40 -0500

    Implemented uninstall script for widget manager

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,21 @@
 /*
 Uninstall script for Widget Manager
 Author:Jason Dark X2
-version: 0.1
+version: 0.95
 Author URI:http://www.jasondarkx2.com/ 
 */ 
+if (
+	!defined( 'WP_UNINSTALL_PLUGIN' )
+||
+	!WP_UNINSTALL_PLUGIN
+||
+	dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) )
+) {
+	status_header( 404 );
+	exit;
+}
+
+delete_option('widgetid');
+
+delete_option('custom-widget');
 ?>
