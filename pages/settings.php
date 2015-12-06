a5285926a7ffc005c78709bef346457b6d4ee53c
Jason Dark <jason@jasondarkx2.com>
2015-12-05 23:41:21 -0500

    Added enable debug mode clickable in settings  page

--- pages/settings.php
+++ pages/settings.php
@@ -5,9 +5,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
 echo '<h1>Settings</h1>';
-$s=get_option('widgetid');
-echo '<div id="debug" hidden="true">';
+echo '<a href="#"  id="debug" title=" enable debug mode">Enable debug mode</a>';
+$s=get_option('custom-widget');
+echo '<div id="debug">';
 echo "<b>DEBUG Section:</b>";
 var_dump($s);
 echo "<h1>widgets</h1>";
