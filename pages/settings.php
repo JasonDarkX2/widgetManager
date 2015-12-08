fd0a6c4053e3c2cb998baf061b0aba8f8f8fa0c5
Jason Dark <jason@jasondarkx2.com>
2015-12-07 21:52:51 -0500

    expanded the debug section

--- pages/settings.php
+++ pages/settings.php
@@ -5,17 +5,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- 
-echo '<h1>Settings</h1>';
-echo '<a href="#"  id="debug" title=" enable debug mode">Enable debug mode</a>';
-$s=get_option('custom-widget');
-echo '<div id="debugSection" hidden="true">';
-echo "<b>DEBUG Section:</b>";
-var_dump($s);
-echo "<h1>widgets</h1>";
-
-echo '</div>';
-//update_option('widgetid', "");
+ ?>
+<h1>Settings</h1>
+<a href="#"  id="debug" title=" enable debug mode">Enable debug mode</a>
+<?php $s=get_option('widgetid');?>
+<div id="debugSection" hidden="true">
+<strong>Options:|</strong><a href="#"  id="wlist" title="Show Widget List">Show Widget List</a>|<a href="#"  id="custlist" title="Show Custom Widget List">Show Custom Widget List</a>|
+<a href="#"  id="debug" title="Clear">Clear all Widgets Data</a>|<a href="#"  id="debug" title="Clear">Clear custom Widgets Data</a>
+<div id="widgetList" hidden="true">
+    <h1>DEBUG Section:</h1>
+<?php var_dump($s);?>
+</div>
+<div id="customw"hidden="true">
+<h1> custom widgets</h1>
+<?php $s=get_option('widgetid');
+var_dump($s);?>
+</div>
+<?php//update_option('widgetid', "");
 //update_option('custom-widget', "");
 
     
