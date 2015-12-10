d8d04b6756c1c2916dc8883ebeef5cfc27588dae
Jason Dark <jason@jasondarkx2.com>
2015-12-10 07:15:02 -0500

    Update the debug section with new script

--- pages/settings.php
+++ pages/settings.php
@@ -1,27 +1,30 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+Plugin Name: Wordpress Widget Manager
+Plugin URI: https://github.com/JasonDarkX2/Wordpress-WidgetManager
+Description:Simply a Wordpress Plugin dedicated to help easily manage custom and default Wordpress widgets
+Author:Jason Dark X2
+version: 0.95
+Author URI:http://www.jasondarkx2.com/ 
+*/ 
  ?>
 <h1>Settings</h1>
 <a href="#"  id="debug" title=" enable debug mode">Enable debug mode</a>
 <?php $s=get_option('widgetid');?>
 <div id="debugSection" hidden="true">
 <strong>Options:|</strong><a href="#"  id="wlist" title="Show Widget List">Show Widget List</a>|<a href="#"  id="custlist" title="Show Custom Widget List">Show Custom Widget List</a>|
-<a href="#"  id="clearAll" title="Clear">Clear all Widgets Data</a>|<a href="#"  id="clearCust" title="Clear">Clear custom Widgets Data</a>
+<a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=caw"  id="clearAll" title="Clear">Clear all Widgets Data</a>|<a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=cw"  id="clearCust" title="Clear">Clear custom Widgets Data</a>
 <div id="widgetList" hidden="true">
-    <h1>DEBUG Section:</h1>
+    <h1>Widget List:</h1>
 <?php var_dump($s);?>
 </div>
 <div id="customw"hidden="true">
 <h1> custom widgets</h1>
-<?php $s=get_option('widgetid');
+<?php $s=get_option('custom-widget');
 var_dump($s);?>
 </div>
-<?php//update_option('widgetid', "");
-//update_option('custom-widget', "");
+<?php
+
+
 
     
