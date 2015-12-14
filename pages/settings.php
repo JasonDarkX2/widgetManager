17122020e0ac3a7e1a51d2e98bb7448472aff7a0
Jason Dark <jason@jasondarkx2.com>
2015-12-13 21:35:43 -0500

    Added a new option to debug mode,  show registered widgets

--- pages/settings.php
+++ pages/settings.php
@@ -12,16 +12,23 @@ Author URI:http://www.jasondarkx2.com/
 <a href="#"  id="debug" title=" enable debug mode">Enable debug mode</a>
 <?php $s=get_option('widgetid');?>
 <div id="debugSection" hidden="true">
-<strong>Options:|</strong><a href="#"  id="wlist" title="Show Widget List">Show Widget List</a>|<a href="#"  id="custlist" title="Show Custom Widget List">Show Custom Widget List</a>|
+<strong>Options:|</strong><a href="#"  id="rwList" title="Show Widget List">Show Registered Widget</a>|<a href="#"  id="wlist" title="Show Widget List">Show Widget List</a>|<a href="#"  id="custlist" title="Show Custom Widget List">Show Custom Widget List</a>|
 <a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=caw"  id="clearAll" title="Clear">Clear all Widgets Data</a>|<a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=cw"  id="clearCust" title="Clear">Clear custom Widgets Data</a>
 <div id="widgetList" hidden="true">
     <h1>Widget List:</h1>
 <?php var_dump($s);?>
 </div>
 <div id="customw"hidden="true">
-<h1> custom widgets</h1>
+<h1> Custom Widgets List:</h1>
 <?php $s=get_option('custom-widget');
 var_dump($s);?>
+</div>
+<div id="rWidgetList"hidden="true">
+    <h1>Registered Widget List:</h1>
+    <?php
+    $wid=($GLOBALS['wp_widget_factory']->widgets);
+                    var_dump($wid);
+    ?>
 </div>
 <?php
 
