ef2992378d07c2c3d10436023299da5d289b5199
Jason Dark <jason@jasondarkx2.com>
2015-12-15 21:20:30 -0500

    Updated the options in debug section

--- pages/settings.php
+++ pages/settings.php
@@ -12,8 +12,11 @@ Author URI:http://www.jasondarkx2.com/
 <a href="#"  id="debug" title=" enable debug mode">Enable debug mode</a>
 <?php $s=get_option('widgetid');?>
 <div id="debugSection" hidden="true">
-<strong>Options:|</strong><a href="#"  id="rwList" title="Show Widget List">Show Registered Widget</a>|<a href="#"  id="wlist" title="Show Widget List">Show Widget List</a>|<a href="#"  id="custlist" title="Show Custom Widget List">Show Custom Widget List</a>|
-<a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=caw"  id="clearAll" title="Clear">Clear all Widgets Data</a>|<a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=cw"  id="clearCust" title="Clear">Clear custom Widgets Data</a>
+<strong>Options:|</strong><a href="#"  id="rwList" title="Show Widget List">Show Registered Widget</a>|
+<a href="#"  id="wlist" title="Show Widget List">Show Widget List</a>|<a href="#"  id="custlist" title="Show Custom Widget List">Show Custom Widget List</a>|
+<a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=caw"  id="clearAll" title="Clear All Widget Data">Clear all Widgets Data</a>|
+<a href="<?php echo plugins_url('actionScripts/debugTool.php', dirname(__FILE__)) ; ?>?op=cw"  id="clearCust" title="Clear custom Widgets Data">Clear custom Widgets Data</a>|
+<a href="#"  id="reload" title="Reload Widgets"> Reload Widgets</a>|
 <div id="widgetList" hidden="true">
     <h1>Widget List:</h1>
 <?php var_dump($s);?>
