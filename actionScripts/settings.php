0c53abd95f162f7ecf67118db6f2e79813bca686
Jason Dark <jason@jasondarkx2.com>
2015-12-25 23:32:34 -0500

    changing widget directory now moves widgets and delete previous directory

--- actionScripts/settings.php
+++ actionScripts/settings.php
@@ -8,25 +8,25 @@
 $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
  require_once( $parse_uri[0] . 'wp-load.php' );
 $dir=$_POST['dir'];
-if($dir!=NULL){
+if(empty($dir)){
+    $dir=dirname(plugin_dir_path(__FILE__)). '/custom-widgets/';
+}
     if(file_exists($dir)==FALSE){
         mkdir($dir,0755);
     }
         $sdir=get_option('widgetdir');
+        If(file_exists($sdir)){
         $contents=scandir($sdir);
         if(SUBSTR($dir, -1) != '/'){$dir.= '/';}
         foreach($contents as $widgets){
             if ($widgets != "." && $widgets != "..") {
-                if(recurse_copy($sdir . $widgets, $dir . $widgets)){
-                   recursiveRemove($sdir . $widgets); 
-                }
+                recurse_copy($sdir . $widgets, $dir . $widgets);
             }
         }
+        recursiveRemove($sdir);
+        echo '<h1>Widget Directory Successfully updated</h1>';
+    }
 update_option('widgetdir',$dir);
-}else{
-    $default=dirname(plugin_dir_path(__FILE__)). '/custom-widgets/';
-    update_option('widgetdir',$default);
-}
 
 
 function recurse_copy($src,$dst) { 
