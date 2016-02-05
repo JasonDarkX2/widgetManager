21d0df3a9d8ab1283612cd0d04a62e9e2e35b4af
Jason Dark <jason@jasondarkx2.com>
2016-02-05 14:55:45 -0500

    Update comments/documentation section

--- actionScripts/debugTool.php
+++ actionScripts/debugTool.php
@@ -1,13 +1,9 @@
 <?php
 /*
-Plugin Name: Wordpress Widget Manager
-Plugin URI: https://github.com/JasonDarkX2/Wordpress-WidgetManager
-Description:Simply a Wordpress Plugin dedicated to help easily manage custom and default Wordpress widgets
-Author:Jason Dark X2
-version: 0.95
-Author URI:http://www.jasondarkx2.com/ 
-*/ 
-
+ * debugTools action script for Wordpress Widget Manager plugin
+ * Handles the debug section of the plugin providing quick options to easily debug without hard coding them in the back end  
+ * For more information check out: http://JasonDarkX2.com/ 
+*/
 $option=$_GET['op'];
 $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
  require_once( $parse_uri[0] . 'wp-load.php' );
