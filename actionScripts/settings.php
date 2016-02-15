dc429014bc3d4a4a4c5da08b997377d5f97f6602
Jason Dark <jason@jasondarkx2.com>
2016-02-15 08:50:49 -0500

    fixed an issue with recursiveRemove function and adjusted notifications

--- actionScripts/settings.php
+++ actionScripts/settings.php
@@ -24,7 +24,7 @@ if(count($presets)>0){
             update_option($option, $v);
         }
     }
-    echo '<div class="notfi"><strong>Settings Successfully Saved</strong></div>';
+    
 }
 if(empty($dir)|| get_option('preset-cdwd')){
     $dir=dirname(plugin_dir_path(__FILE__)). '/custom-widgets/';
@@ -45,19 +45,21 @@ if(empty($dir)|| get_option('preset-cdwd')){
             }
         }
         
-        if($sdir!= get_option('widgetdir')){
+        if($sdir!=$dir){
         $check=recursiveRemove($sdir);
         }
         if($dir!=get_option('widgetdir')){
         echo '<br/> <div class="notfi"><strong>Widget upload directory Successfully changed</strong></div>';
         }
-        if($check==FALSE && $dir!=get_option('widgetdir')){
+         elseif($check==FALSE && $dir!=get_option('widgetdir')){
             echo '<div class="errorNotfi"><strong> An error occured while changing Widget upload directory please check directory file permissions</strong></div>';
             echo get_option('widgetdir');
+        }else{
+            echo '<div class="notfi"><strong>Settings Successfully Saved</strong></div>';
         }
     }
 update_option('widgetdir',$dir);
- echo '<div class="notfi"><strong>Settings Successfully Saved</strong></div>';
+ //echo '<div class="notfi"><strong>Settings Successfully Saved</strong></div>';
 
 function recurse_copy($src,$dst) { 
     $dir = opendir($src); 
@@ -77,11 +79,9 @@ function recurse_copy($src,$dst) {
 
 function recursiveRemove($dir)
 {
-    var_dump(debug_backtrace());
-    echo $dir;
     foreach(glob("{$dir}/*") as $file)
     {
-        if(is_dir($file)) { 
+        if(is_dir($file)) {
             recursiveRemove($file);
         } else {
             unlink($file);
