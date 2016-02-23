121fce2bdac14622f86056521e35005c26805656
Jason Dark <jason@jasondarkx2.com>
2016-02-23 16:20:44 -0500

    Separated permission and ownership checks, adjusted  messages

--- widgetManager.php
+++ widgetManager.php
@@ -310,15 +310,26 @@ $pdir= plugin_dir_path( __FILE__);
  $user=exec(whoami);
  $errors =array();
  
+ 
  if($user==fileowner($dir)){
-     array_push($errors,'<li>Problem with widget directory permissions:<br/> '
+     array_push($errors,'<li>Problem with widget directory ownership:<br/> '
      .  'please change file owner to <strong>'. $user . '</strong> for <strong>' . $dir 
              . '</strong></br>You can use the following command:<br/><strong>sudo chown '. $user . ' '. $dir .'</strong></li>');
  }
   if($user==fileowner($pdir)){
-     array_push($errors,'<li>Problem with  plugin directory permissions:<br/> '
+     array_push($errors,'<li>Problem with the  plugin directory ownership:<br/> '
      .  'please change  directory owner to <strong>'. $user . '</strong> for <strong>' . $dir 
-             . '</strong></br>You can use the following command:<br/><strong>sudo chown '. $user . ' '. $dir .'</strong></li>');
+             . '</strong></br>You can use the following command:<br/><strong>sudo chown '. $user . ' '. $pdir .'</strong></li>');
+ }
+ if(substr(sprintf('%o', fileperms($dir)), -4) !='0755'){
+     array_push($errors,'<li>Problem with widget upload directory permissions:<br/> '
+     .  'please change  directory permission to <strong>'.'755' . '</strong> for <strong>' . $dir 
+             . '</strong></br>You can use the following command:<br/><strong>sudo chmod '. '755' . ' '. $dir .'</strong></li>');
+ }
+ if(substr(sprintf('%o', fileperms($pdir)), -4) !='0755'){
+     array_push($errors,'<li>Problem with the plugin directory permissions:<br/> '
+     .  'please change  directory permission to <strong>'.'755' . '</strong> for <strong>' . $dir 
+             . '</strong></br>You can use the following command:<br/><strong>sudo chmod '. '755' . ' '. $pdir .'</strong></li>');
  }
  if(count($errors)>0){
  echo '<div class="errorNotfi"><ul>';
