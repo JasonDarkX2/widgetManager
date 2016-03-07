057c944b2a79c4160016cb67c9d1564488da536b
Jason Dark <jason@jasondarkx2.com>
2016-03-07 16:27:08 -0500

    Added a successful deletion check

--- actionScripts/cwdelete.php
+++ actionScripts/cwdelete.php
@@ -14,24 +14,26 @@ $wdir=get_option('widgetdir');
 if(file_exists($wdir .'/' .$custwid[$widgetid]['file'])===TRUE){
      $toDel=explode("/",$custwid[$widgetid]['file']);
      $del= $wdir . '/' . $toDel[0];
+     $success=false;
 if(is_dir($del)===TRUE){
  $objects = scandir($del);
      foreach ($objects as $object) {
        if ($object != "." && $object != "..") {
-         if (filetype($del."/".$object) == "dir") recursiveRemove($del."/".$object); else unlink($del."/".$object);
+         if (filetype($del."/".$object) == "dir") $success=recursiveRemove($del."/".$object); else $success=unlink($del."/".$object);
        }
      }
-     rmdir($del);
+     $success=rmdir($del);
 }else{
-unlink($wdir . '/' . $custwid[$widgetid]['file']);
+$success=unlink($wdir . '/' . $custwid[$widgetid]['file']);
 }
 }
+if($success==TRUE){
 unset($custwid[$widgetid]);
 unset($widgets[$widgetid]);
 update_option('custom-widget', $custwid);
 update_option('widgetid', $widgets);
 echo '<div class="notfi">deleted '. $widgetid .'</div>';
-
+}
 function recursiveRemove($dir)
 {
     foreach(glob("{$dir}/*") as $file)
@@ -39,8 +41,10 @@ function recursiveRemove($dir)
         if(is_dir($file)) { 
             recursiveRemove($file);
         } else {
-            unlink($file);
+            $sucess=unlink($file);
         }
     }
-    rmdir($dir);
-}
+    $sucess=rmdir($dir);
+
+    return $sucess;
+        }
