9feadb1580b46a97475fe9c7d6653bfd7f56808f
Jason Dark <jason@jasondarkx2.com>
2016-03-08 19:41:17 -0500

    Changed widget delete option  action to credential page

--- pages/customWidgets.php
+++ pages/customWidgets.php
@@ -30,7 +30,7 @@
     <h2><strong>Custom Widgets Option</strong></h2>
     <?php permissionChecker();?>
     <table border="1px;"><tr><th>Custom Widgets</th><th>filename</th><th>Register Custom Widget</th><th>UnRegister Custom Widget</th><th>Extra options</th></tr>
-        <form id="settingsop" method="POST" action="<?php echo plugins_url('actionScripts/customWidgetOptions.php', dirname(__FILE__)) ; ?>">
+        <form id="settingsop" method="POST" action="<?php menu_page_url( 'credentials' ); //for reference-> echo plugins_url('actionScripts/customWidgetOptions.php', dirname(__FILE__)) ; ?>">
             <input type="hidden" name="wpdir" value="<?php echo basename(content_url());?>" />
     <?php 
     if($custwid==''|| empty($custwid)){
