43791326d1dd300a0dc9cd1e86edfb9c1cf9de01
Jason Dark <jason@jasondarkx2.com>
2016-03-14 18:41:15 -0400

    Code clean up

--- pages/customWidgets.php
+++ pages/customWidgets.php
@@ -4,20 +4,7 @@
  * Handles the displaying of custom widgets and their options
  * For more information check out: http://JasonDarkX2.com/ 
 */
-    $dir=plugin_dir_path( __FILE__ ).'/custom-widgets';
-    empty_names();
-    $custwid= get_option('custom-widget')?>
-<?php if(false === ($credentials = request_filesystem_credentials($url, $method, false, $context, $fields))) 
-  {
-    return false;
-  }
-
-  //check if credentials are correct or not.
-  if(!WP_Filesystem($credentials)) 
-  {
-    request_filesystem_credentials($url, $method, true, $context);
-    return false;
-  } ?>
+?>
 <form>
 <div id="dialog" hidden="true">
   <p>Add or Import your Custom widgets below.... </p>
