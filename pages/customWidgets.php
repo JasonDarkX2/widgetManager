686f5730d2a5c7456147ec0fb65f4909aeaae28e
Jason Dark <jason@jasondarkx2.com>
2016-03-15 16:49:13 -0400

    simple correction

--- pages/customWidgets.php
+++ pages/customWidgets.php
@@ -17,7 +17,7 @@
     <h2><strong>Custom Widgets Option</strong></h2>
     <?php permissionChecker();?>
     <table border="1px;"><tr><th>Custom Widgets</th><th>filename</th><th>Register Custom Widget</th><th>UnRegister Custom Widget</th><th>Extra options</th></tr>
-        <form id="settingsop" method="POST" action="<?php menu_page_url( 'credentials' ); //for reference-> echo plugins_url('actionScripts/customWidgetOptions.php', dirname(__FILE__)) ; ?>">
+        <form id="settingsop" method="POST" action="<?php echo plugins_url('actionScripts/customWidgetOptions.php', dirname(__FILE__)) ; ?>">
             <input type="hidden" name="wpdir" value="<?php echo basename(content_url());?>" />
     <?php 
     if($custwid==''|| empty($custwid)){
@@ -31,7 +31,7 @@
             <input type='hidden' name='customWidget[]' value='<?php echo  $c['key'] ?>' id='customWidget'> 
             <input type="radio" name="<?php echo$c['key'];?>" <?php checked(1,$c['status'] ); ?> value="true"></td>
         <td><input type="radio" name="<?php echo $c['key'];?>" <?php checked('',$c['status'] ); ?> value="false"></td>
-        <td><a class="deleteWid" href="<?php echo plugins_url('actionScripts/cwdelete.php', dirname(__FILE__)) ; ?>?w=<?php echo$c['key']; ?>&wpdir=<?php echo basename(content_url());?>" title="delete <?php echo$c['name']; ?>">Delete Widget</a></td>
+        <td><a class="deleteWid" href="<?php menu_page_url( 'credentials' ); //for reference->plugins_url('actionScripts/cwdelete.php', dirname(__FILE__)) ; ?>?w=<?php echo$c['key']; ?>&wpdir=<?php echo basename(content_url());?>" title="delete <?php echo$c['name']; ?>">Delete Widget</a></td>
       <?php endif;?>
     </tr>
     <?php endforeach; }?>
