4526f007266a7b99777f03a5ce67b951afb56643
Jason Dark <jason@jasondarkx2.com>
2016-04-25 17:38:45 -0400

    Prototyping  UI changes

--- pages/Manager.php
+++ pages/Manager.php
@@ -11,7 +11,7 @@
     <?php settings_fields( 'WM-setting' ); ?>
     <?php do_settings_sections( 'WM-setting' ); ?>
     <table border='1px' >
-        <tr><th> Widgets</th><th>Type</th><th> Enabled</th><th>Disabled</th></tr>
+        <tr><th> Widgets</th><th>Type</th><th> Enabled/Disabled</th></tr>
     <?php  
     
     $widgets=get_option('widgetid');
@@ -31,8 +31,18 @@
             <td><input type='hidden' name='widgetid[]' value='<?php echo  $widget['key'] ?>' id='widgetId'> 
                 <?php echo '<strong>'. $widget['name'] .'</strong><br/>' . $widget['Description']; ?></td>
             <td><?php echo $widget['type']; ?></td>
-            <td><input type="radio" name="<?php echo $widget['key']; ?>" value="enable" <?php  checked(1,$widget['status']); ?> ><?php //echo get_option($widget['key']);?></td>
-            <td><input type="radio" name="<?php echo $widget['key'];?>" <?php checked('',$widget['status'] ); ?> value="disable"></td>
+            <!--<td><input type="radio" name="<?php echo $widget['key']; ?>" value="enable" <?php  checked(1,$widget['status']); ?> ><?php //echo get_option($widget['key']);?></td>
+            <td><input type="radio" name="<?php echo $widget['key'];?>" <?php checked('',$widget['status'] ); ?> value="disable"></td>!-->
+            <td>
+                <div class="switch-field">
+      <div class="switch-title">Set the status of widget:</div>
+      <input type="radio" id="switch_left_<?php echo $widget['key']; ?>" name="<?php echo $widget['key']; ?>" value="enabled" <?php checked(1,$widget['status']); ?>/>
+      <label for="switch_left_<?php echo $widget['key']; ?>">Enable</label>
+      <input type="radio" id="switch_right_<?php echo $widget['key']; ?>" name="<?php echo $widget['key']; ?>" value="disable" <?php checked('',$widget['status'] ); ?>/>
+      <label for="switch_right_<?php echo $widget['key']; ?>">Disable</label>
+    </div>
+                
+            </td>
         </tr>
         <?php endforeach;?>
         <tr>
