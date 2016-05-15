7fad404763774d2ecd6cb88806b3397343bb3646
Jason Dark <jason@jasondarkx2.com>
2016-05-15 10:59:53 -0400

    add check box field to customdirform

--- pages/Manager.php
+++ pages/Manager.php
@@ -75,13 +75,16 @@ session_start();
         <input type="hidden" id="wpdir" name="wpdir" value="<?php echo basename(content_url()); ?>" />
     </form>
     <form id="customDirForm" method="POST" action="<?php echo plugins_url('actionScripts/settings.php', dirname(__FILE__));?>">
-    <input type="hidden" name="wpdir" value="<?php echo basename(content_url());?>" />
     <strong>Plugin Upload Directory:</strong>
     <?php if(get_option('preset-cdwd')==FALSE) : ?>
     <input type="text" name="dir" id="widgetdir" size="80" value="<?php echo str_replace('\\', '/',get_option('widgetdir'));?>">
     <?php else:?>
     <input type="text" name="dir" size="80" id="widgetdir"  disabled value="<?php echo str_replace('\\', '/',get_option('widgetdir'));?>">
     <?php endif;?>
+   <br/>
+   <input type='hidden' name='preset[]' value='cdwd' id='preset'> 
+   <input type="checkbox" name="cdwd" id="cdwd"  value="true"  <?php checked(get_option('preset-cdwd'),1);?>/>Use default Custom Widget Directory.
+    <input type="hidden" id="wpdir" name="wpdir" value="<?php echo basename(content_url()); ?>" />
     <p>
     </form>
 </div>
