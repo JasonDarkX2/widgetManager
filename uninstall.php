b0817602ec4cb91f31ac819283e2907f93b5382f
Jason Dark <jason@jasondarkx2.com>
2016-05-19 17:28:40 -0400

    added delete option for widgetdir, and code formating

--- uninstall.php
+++ uninstall.php
@@ -1,21 +1,22 @@
 <?php
+
 /*
  * Uninstall  script for Wordpress Widget Manager plugin
  * Simply handles uninstalling of plugin
  * For more information check out: http://JasonDarkX2.com/ 
-*/
+ */
 if (
-	!defined( 'WP_UNINSTALL_PLUGIN' )
-||
-	!WP_UNINSTALL_PLUGIN
-||
-	dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) )
+        !defined('WP_UNINSTALL_PLUGIN') ||
+        !WP_UNINSTALL_PLUGIN ||
+        dirname(WP_UNINSTALL_PLUGIN) != dirname(plugin_basename(__FILE__))
 ) {
-	status_header( 404 );
-	exit;
+    status_header(404);
+    exit;
 }
 
 delete_option('widgetid');
 
 delete_option('custom-widget');
+
+delete_option('widgetdir');
 ?>
