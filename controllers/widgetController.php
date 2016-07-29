261dea685efae9f3ad1c48aca2e6a77fe11a39d2
Jason Dark <jason@jasondarkx2.com>
2016-07-29 13:31:08 -0400

    some adjustments

--- controllers/widgetController.php
+++ controllers/widgetController.php
@@ -10,7 +10,7 @@ class widgetController{
    function load_widgets() {
        $theWidget= new theWidget();
         $w = get_option('widgetid');
-        if (empty($w)) {
+        //if (empty($w)) {
             $widgets = array_keys($GLOBALS['wp_widget_factory']->widgets);
             $w = ($GLOBALS['wp_widget_factory']->widgets);
             foreach ($widgets as $keys) {
@@ -27,15 +27,15 @@ class widgetController{
             }
             update_option('widgetid', $widgetsId);
             $widgets = $widgetsId;
-        } else {
-            /*$widgets = array_keys($GLOBALS['wp_widget_factory']->widgets);
+        /*} else {
+            $widgets = array_keys($GLOBALS['wp_widget_factory']->widgets);
             $wid = ($GLOBALS['wp_widget_factory']->widgets);
             $type = get_type($keys);
             array_push($w, $w[$keys] = array('key' => $keys, 'name' => get_name($keys), 'Description' => get_description($keys), 'id' => get_id($keys), 'type' => $type, 'status' => TRUE));
             array_pop($w);
 
-            update_option('widgetid', $w);*/
-        }
+            update_option('widgetid', $w);
+        }*/
     }
         function disable_plugin_widget() {
         $d = get_option('widgetid');
