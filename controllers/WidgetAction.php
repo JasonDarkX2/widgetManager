bb633ae5355ea8289be99f12b03d85301f607673
Jason Dark <jason@jasondarkx2.com>
2016-08-30 19:49:33 -0400

    Rewrote into a controller to widget status requests

--- controllers/WidgetAction.php
+++ controllers/WidgetAction.php
@@ -1,143 +1,89 @@
 <?php
 
 /*
-  setter class for Wordpress Widget Manager
+ * WidgetManager
+  widget controller for the setting widgets state
  */
+require_once dirname(dirname(__FILE__)) . '/model/WidgetSetter.php';
 
-class WidgetAction{
-    
-    function __construct() {
-        
-    }
-    function delete_widget() {
 
-    $_POST['wpdir'] = $_GET['wpdir'];
-    $_POST['w'] = $_GET['w'];
-    $form_fields = array('wpdir', 'w');
-    $nonce = $_REQUEST['_wpnonce'];
-    $name = 'delete-' . $_GET['w'];
-    if (wp_verify_nonce($nonce, $name) == 2) {
-        die();
-        header('Location: ' . menu_page_url('widgetM') . '&del=true');
-    }
-    if (connect_fs($url, "POST", get_option('widgetdir'), $form_fields)) {
-        //deletion  process
-        global $wp_filesystem;
-        $custwid = get_option('custom-widget');
-        $widgets = get_option('widgetid');
-        $widgetid = $_GET['w'];
-        $wdir = get_option('widgetdir');
-        if (file_exists($wdir . '/' . $custwid[$widgetid]['file']) === TRUE) {
-            $toDel = explode("/", $custwid[$widgetid]['file']);
-            $del = $wdir . $toDel[0];
-            display_msg($wp_filesystem->rmdir($del, true), TRUE);
-            header('Location: ' . menu_page_url('widgetM') . '&del=true');
-            session_start();
-            $_SESSION['deletion'] = display_msg($wp_filesystem->rmdir($del, true), TRUE);
-        }
-    } else {
-        
-    }
-}
+$w = $_POST['wpdir'];
+$parse_uri = explode($w, $_SERVER['SCRIPT_FILENAME']);
+require_once( $parse_uri[0] . 'wp-load.php' );
+$wid=get_option('widgetid');
+$WidgetSetter = new WidgetSetter();
+$enablecon = 0;
+$disabledcon = 0;
+$widgets = $_POST['widgetid'];
+If (isset($_POST['quickOp'])) {
+    switch ($_POST['quickOp']) {
+        case 'enbwid':
+            $widgets = get_option('widgetid');
+            $WidgetSetter->setAllWidget($widgets,TRUE);
+            break;
+        case 'diswid':
+            $widgets = get_option('widgetid');
+            $WidgetSetter->setAllWidget($widgets,FALSE);
+            break;
+        case 'disDefault':
+            $widgets = get_option('widgetid');
+            $WidgetSetter->setAllWidget($widgets,TRUE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Default", FALSE);
+            break;
+        case 'enbDefault':
+            $widgets = get_option('widgetid');
+            $WidgetSetter->setAllWidget($widgets,TRUE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Plugin",FALSE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Custom",FALSE);
+            break;
+                case 'enbCust':
+            $widgets = get_option('widgetid');
+            $WidgetSetter->setAllWidget($widgets,TRUE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Default",FALSE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Plugin",FALSE);
+            break;
+        case 'disCust':
+            $widgets = get_option('widgetid');
+             $WidgetSetter->setAllWidget($widgets,TRUE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Custom",FALSE);
+            break;
+        case 'enbPlugin':
+            $widgets = get_option('widgetid');
+             $WidgetSetter->setAllWidget($widgets,TRUE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Default",FALSE);
+            $WidgetSetter->setByWidgetTypes($widgets, "Custom",FALSE);
 
-function add_widget() {
-    $name = $_FILES["widgetToUpload"]['name'];
-    $tmp = $_FILES['widgetToUpload']["tmp_name"];
+            break;
+        case 'disPlugin':
+            $widgets = get_option('widgetid');
+            enable_all($widgets);
+            $WidgetSetter->setByWidgetTypes($widgets, "Plugin",FALSE);
+            break;
+        case 'pick':
+            return;
+            break;
+        default:
+            $WidgetSetter->status_count($wid,$enablecon, $disabledcon);
+            break;
+    }
+} else {
 
-    $dest = wp_upload_dir();
-    if ($name == null) {
-        //session_start();
-        $name = $_SESSION['name'];
+    $wid = get_option('widgetid');
+    if (empty($widgets)) {
+        return;
     }
-    if ($name != null) {
-        $destination = $dest['basedir'] . '/' . $name;
-        move_uploaded_file($tmp, $destination);
-        $file = str_replace('//', '/', str_replace('\\', '/', $destination));
-        $_POST['file'] = $file;
-        //session_start();
-        $_SESSION['name'] = $name;
-        $form_fields = array('file');
-    }
-    if (connect_fs('', "POST", get_option('widgetdir'), $form_fields)) {
-        $destination = get_option('widgetdir');
-        $file = $_POST['file'];
-        $unzip = unzip_file($file, $destination);
-        if (is_wp_error($unzip)) {
-            //session_start();
-            $_SESSION['errors'] = ' <div class="errorNotfi">' . $unzip->get_error_message() . '</div>';
-        } else {
-            $_SESSION['errors'] = NULL;
-        }
-        header('Location: ' . menu_page_url('widgetM'));
-        unlink($file);
-    }
-}
+    $option = 0;
+    $data = $_POST;
+    if (isset($data[$widgets])) {
+        $option = $data[$widgets];
+        $wid[$widgets]['status']=($option == 'enable') ? TRUE:FALSE; 
 
-function display_msg($output, $del) {
-    if ($del) {
-        $msgType = "Deleted";
     }
-    if ($output == true) {
-        return '<div class="notfi">Successfully ' . $msgType . ' </div>';
-    } else if (is_wp_error($output) && $output != NULL) {
-        ?>
-        <div class="errorNotfi"><?php $output->get_error_message(); ?></div>
-        <div><a href="<?php menu_page_url('cwop') ?>">Return to Custom Widgets Options</a>|<a href="<?php menu_page_url('widgetM') ?>">Return to Widgets Manager</a></div>
-    <?php } else { ?>
-        <div class="errorNotfi"> Unable to perform action</div>
-        <div><a href="<?php menu_page_url('cwop') ?>">Return to Custom Widgets Options</a>|<a href="<?php menu_page_url('widgetM') ?>">Return to Widgets Manager</a></div>
-        <?php
-    }
-}
-
-function enable_all(&$widgets) {
-    foreach ($widgets as $widgetId) {
-
-        $widgets[$widgetId['key']]['status'] = TRUE;
-        update_option('widgetid', $widgets);
-    }
-}
-
-function disable_all(&$widgets) {
-    foreach ($widgets as $widgetId) {
-        $widgets[$widgetId['key']]['status'] = false;
-        update_option('widgetid', $widgets);
-    }
-}
-
-function disable_types(&$w, $type) {
-    foreach ($w as $wid) {
-        if ($wid['type']!= $type){ 
-            continue;
-        }else{
-            $w[$wid['key']]['status'] = FALSE;
-        }
-    }
-    update_option('widgetid', $w);
-}
-
-function get_count($type) {
-
-    $w = get_option('widgetid');
-    $count = 0;
-    foreach ($w as $wid) {
-        if (strtolower($wid['type']) == strtolower($type)) {
-            $count++;
-        }
-    }
-    return $count;
-}
-
-function status_count($wl,&$enablecon, &$disabledcon){ 
-    foreach ($wl as $wid) {
-        if ($wid['status']){ 
-            $enablecon ++;
-        } else {
-            $disabledcon ++;
-        }
-    }
-}
-    
+    update_option('widgetid', $wid);
     
 }
+$wid=get_option('widgetid');
+$WidgetSetter->status_count($wid,$enablecon, $disabledcon);
+echo '<div class="notfi">' . $enablecon . ' enabled widgets and ' . $disabledcon . ' disabled widgets' . '</div>';
+
 ?>
