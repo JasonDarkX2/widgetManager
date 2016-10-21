cfbf4964456edde55ab15e226e9f8da62170bbfc
Jason Dark <jason@jasondarkx2.com>
2016-10-20 22:16:50 -0400

    modal view placeholder

---
+++ views/modal.php
@@ -0,0 +1,9 @@
+<!-- The Modal -->
+<div id="deleteModal" class="modal">
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">x</span>
+ <?php 
+    <p></p>
+  </div>
+</div>
