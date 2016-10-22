c1bd500bd82b03f42cd974de43a9ab2cd949dea1
Jason Dark <jason@jasondarkx2.com>
2016-10-22 10:49:50 -0400

    update modal

--- views/modal.php
+++ views/modal.php
@@ -2,8 +2,12 @@
 <div id="deleteModal" class="modal">
   <!-- Modal content -->
   <div class="modal-content">
+      <h1>Delete Widget?</h1>
     <span class="close">x</span>
  <?php 
+ $url = plugins_url( '/controllers/widgetAdder_controller.php',  dirname(__FILE__));
+ var_dump($url);
+  $credentials=request_filesystem_credentials($url); ?>
     <p></p>
   </div>
 </div>
